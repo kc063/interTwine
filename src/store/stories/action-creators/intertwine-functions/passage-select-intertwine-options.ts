71b752e9c95e582d7d11d172b97ec4db291ef04f
Jack Cobey <katherine_cobey@brown.edu>
2023-04-26 21:49:14 -0400

    async function letsgoooo

--- src/store/stories/action-creators/intertwine-functions/passage-select-intertwine-options.ts
+++ src/store/stories/action-creators/intertwine-functions/passage-select-intertwine-options.ts
@@ -7,10 +7,48 @@ import {Passage, Story} from "../../stories.types";
  * @param passage
  * @param exclusive
  */
-export function onSelectPassage(story: Story,
+export async function onSelectPassage(story: Story,
     passage: Passage,
-    exclusive: boolean){
-
+    exclusive: boolean): Promise<boolean> {
+  //appropriate backend call
+  fetch("http://localhost:3232/passages?id=" + passage.id)
+  .then((response: Response) => response.json())
+  .then(
+      (responseObject: any) => {
+        if (isSelectSuccessResponse(responseObject)) {
+          if(isPassage(responseObject.data)){
+            console.log(responseObject.data);
+            //TODO: UPDATE PASSAGE!
+          }
+          else{
+            console.log("Error: response was success, but data was malformed passage.")
+          }
+        }
+        else if (isSelectClaimedResponse(responseObject)) {
+          if(isPassage(responseObject.data)){
+            console.log(responseObject.data);
+            //TODO: UPDATE PASSAGE!
+          }
+          else{
+            console.log("Error: response was claimed, but data was malformed passage.")
+          }
+        }
+        else if (isSelectDeleteResponse(responseObject)) {
+          console.log(responseObject.result);
+          //TODO: DELETE PASSAGE!
+        }
+        else {
+          console.log("Error: bad response type.");
+        }})
+  //logic for this:
+  //1: backend call
+  //2: wait for returned data from backend call
+  //3: IF returned object gives go-ahead, return true (can select)
+  //4: ELSE update the object, either because it has been deleted
+  //write delete clause
+  //   or claimed...
+  //write claim clause
+  return true;
 }
 
 /**
@@ -44,3 +82,95 @@ export function onNewPassage(story: Story, passage: Passage, exclusive: boolean)
 export function onDeletePassage(story: Story, passage: Passage, exclusive: boolean){
 
 }
+
+//*** NARROWERS ***/
+
+/**
+ * Returns  if select success response is selectful and successful.
+ * @param rjson the json to check
+ * @returns whether the format is that of a successful response
+ */
+function isSelectSuccessResponse(rjson: any): rjson is selectSuccessResponse {
+  if (!("result" in rjson)) return false;
+  if (rjson["result"] === "success") {
+    if (!("data" in rjson)) return false;
+  } else {
+    return false;
+  }
+  return true;
+}
+
+/**
+ * Returns if select response is claimed.
+ * @param rjson the json to check
+ * @returns whether the format is that of a claim response.
+ */
+function isSelectClaimedResponse(rjson: any): rjson is selectClaimedResponse {
+  if (!("result" in rjson)) return false;
+  if (rjson["result"] === "error_claimed") {
+    if (!("data" in rjson)) return false;
+    }
+  else {
+      return false;
+  }
+  return true;
+}
+
+/**
+ * Returns if select response is claimed.
+ * @param rjson the json to check
+ * @returns whether the format is that of a claim response.
+ */
+function isSelectDeleteResponse(rjson: any): rjson is selectDeleteResponse {
+  if (!("result" in rjson)) return false;
+  if (rjson["result"] === "error_datasource") {
+    return true;
+  }
+  return false;
+}
+
+
+/**
+ * passage narrower :|
+ */
+/**
+ * Returns whether rjson matches the json format of the expected redlining
+ * server success response.
+ *
+ * @param rjson the json to check
+ * @returns whether the format is that of a successful response
+ */
+function isPassage(rjson: any): rjson is Passage {
+  if (!("height" in rjson)) return false;
+  if (!("highlighted" in rjson)) return false;
+  if (!("id" in rjson)) return false;
+  if (!("left" in rjson)) return false;
+  if (!("name" in rjson)) return false;
+  if (!("selected" in rjson)) return false;
+  if (!("story" in rjson)) return false;
+  if (!("tags" in rjson)) return false;
+  if (!("text" in rjson)) return false;
+  if (!("top" in rjson)) return false;
+  if (!("width" in rjson)) return false;
+  if (!("claimed" in rjson)) return false;
+  if (!("user" in rjson)) return false;
+  return true;
+}
+
+/**
+ * interface for having pins
+ */
+interface selectSuccessResponse{
+  result: string;
+  data: Passage;
+}
+
+interface selectClaimedResponse{
+  result: string;
+  data: Passage;
+}
+
+interface selectDeleteResponse{
+  result: string;
+}
+
