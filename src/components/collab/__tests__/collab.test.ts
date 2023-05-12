caf5b8826c4ed9b02ba63cdbd3fb8d7d59084359
Prashanthi Ramachandran <prashirams99@gmail.com>
2023-05-12 12:18:21 -0400

    push demo branch

--- src/components/collab/__tests__/collab.test.ts
+++ src/components/collab/__tests__/collab.test.ts
@@ -1,14 +1,23 @@
-import {render, screen} from '@testing-library/react';
-import * as React from 'react';
-import {Routes} from '../../../routes';
-import {createHashHistory} from 'history';
-import {PrefsContext, PrefsContextProps} from '../../../store/prefs';
-import {fakePrefs} from '../../../test-util';
-import {
-	mockStory1,
-	mockStory2,
-	mockStory3
-} from '../../../store/stories/action-creators/intertwine-functions/mocks';
-import {Auth0Provider, useAuth0} from '@auth0/auth0-react';
-import '@testing-library/jest-dom';
-import {Mock} from 'jest-mock';
+// import {render, screen} from '@testing-library/react';
+// import * as React from 'react';
+// import {Routes} from '../../../routes';
+// import {createHashHistory} from 'history';
+// import {PrefsContext, PrefsContextProps} from '../../../store/prefs';
+// import {fakePrefs} from '../../../test-util';
+// import {
+// 	mockStory1,
+// 	mockStory2,
+// 	mockStory3
+// } from '../../../store/stories/action-creators/intertwine-functions/mocks';
+// import {Auth0Provider, useAuth0} from '@auth0/auth0-react';
+// import '@testing-library/jest-dom';
+// import {Mock} from 'jest-mock';
+// import { ViewEditors } from '../ViewEditors';
+
+// describe('<DialogCard>', () => {
+// 	function renderComponent() {
+// 		return (
+
+// 		);
+// 	}
+// );
